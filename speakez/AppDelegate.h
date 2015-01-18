50a082f5fd0b98a7f6b536b7677db433eae38820
Edward Gheorghita <Edward@Edwards-MacBook-Pro.local>
2015-01-18 10:30:12 -0600

    Initial Commit

---
+++ speakez/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  speakez
+//
+//  Created by Edward Gheorghita on 1/18/15.
+//  Copyright (c) 2015 Nick Gheorghita. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ speakez/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  speakez
+//
+//  Created by Edward Gheorghita on 1/18/15.
+//  Copyright (c) 2015 Nick Gheorghita. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
